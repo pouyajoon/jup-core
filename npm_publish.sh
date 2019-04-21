35d98089f5f874f8be9f2cc9337713c0aff57fe2
pouya mohtacham <pouyajoon@gmail.com>
2019-04-21 22:07:26 +0200

    update publish

--- npm_publish.sh
+++ npm_publish.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-git add .
-git commit -a -m "$1"
-git push origin master
+# git add .
+# git commit -a -m "$1"
+# git push origin master
+
+npm run build
+
+cp -r dist/* publish/
+cp -r package.json publish/
+cp -r README.md publish/
 npm version minor
 npm publish
