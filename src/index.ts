e39a6bcc906688fa9174b5e0e7d923f5e5c10237
pouya mohtacham <pouyajoon@gmail.com>
2019-04-22 10:23:18 +0200

    add merge methods

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { a } from './a'
 export { camelize, capitalize, plural, singularize, toUnderscore } from './utils/stringUtils'
+export { mergeArrayOfArray, chunk, mergeOnAnObjectByName, mergeValueByName } from './utils/arrayUtils'
---
+++ src/utils/arrayUtils.ts
@@ -0,0 +1,25 @@
+export function mergeArrayOfArray<T>(a: T[][]) {
+    return [].concat.apply([], a) as T[];
+}
+
+export function chunk<T>(list: T[], size: number): T[][] {
+    return list.reduce(
+        (all: any, one: any, i) => {
+            const ch = Math.floor(i / size);
+            all[ch] = [].concat((all[ch] || []), one);
+            return all;
+        },
+        []);
+}
+
+interface IObjectWithName { name: string }
+interface IObjectWithNameAndValue { name: string, value: string }
+export function mergeOnAnObjectByName(acc: Record<string, IObjectWithName>, v: IObjectWithName) {
+    acc[v.name] = v;
+    return acc;
+}
+
+export function mergeValueByName(acc: Record<string, string>, v: IObjectWithNameAndValue) {
+    acc[v.name] = v.value;
+    return acc;
+}
