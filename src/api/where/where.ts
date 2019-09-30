01e4bba4c9c6b99a1e964a50364ac525836c8e1e
pouya mohtacham <pouyajoon@gmail.com>
2019-09-30 21:27:42 +0200

    update where filter type:

--- src/api/where/where.ts
+++ src/api/where/where.ts
@@ -1,5 +1,5 @@
 import { JupWhereOperator, IJupWhereNode } from './where.models';
 
-export function createFilter(operator: JupWhereOperator, column: string, value: string): IJupWhereNode {
+export function createFilter(operator: JupWhereOperator, column: string, value: string | number): IJupWhereNode {
     return { operator, column, value };
 }
