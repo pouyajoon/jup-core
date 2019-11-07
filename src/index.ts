7454fed625949d6344353b3909513d29e5cce2cd
pouya mohtacham <pouyajoon@gmail.com>
2019-11-07 11:46:42 +0100

    update imports

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,7 @@ export { camelize, capitalize, toPlural, singularize, toUnderscore } from './uti
 export * from './utils/arrayUtils';
 export { rwait, wait } from './utils/timeUtils';
 export { mapper, getMapperMatch } from './utils/mapper';
-import { IMapperMap } from './utils/mapper';
-export type IMapperMap<A, B> = IMapperMap<A, B>;
-
 export { PropertyTypeJupType } from './metaModel/propertyTypes';
-
 export { createFilter } from './api/where/where';
 export { IJupWhereNode, JupWhereRecord, JupWhereOperator, IJupWhereRecordValue } from './api/where/where.models';
 export * from './packages/point/point';
@@ -15,5 +11,4 @@ export * from './packages/area/area';
 export * from './packages/area/distance';
 export * from './packages/area/perimeter';
 export * from './packages/geometry/geometry';
-import { ColorsMap } from './packages/colors/colors.models';
-export type ColorsMap = ColorsMap;
+export * from './packages/colors/colors.models';
