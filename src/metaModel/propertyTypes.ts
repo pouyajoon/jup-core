c5b5251961e1c2dd9164edab3fefae7977c03c15
pouya mohtacham <pouyajoon@gmail.com>
2019-12-16 09:57:07 +0100

    update types

--- src/metaModel/propertyTypes.ts
+++ src/metaModel/propertyTypes.ts
@@ -1,3 +1,3 @@
 export type PropertyTypeJupType = "string" | "float" | "square-image" | "timestamp"
     | "image" | "address" | "integer" | "primary-key" | "icon" | "color" | "date" | "foreign-key"
-    | "belongs-to" | "has-many" | "has-many-paginated" | "boolean" | "surface" | "distance" | "point2d";
+    | "belongs-to" | "has-many" | "has-many-paginated" | "boolean" | "surface" | "distance" | "point2d" | "text";
