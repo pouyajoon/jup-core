c2de17aec4a9564cfece0a409e3dfedfa612c550
pouya mohtacham <pouyajoon@gmail.com>
2019-12-16 13:48:37 +0100

    add currency type

--- src/metaModel/propertyTypes.ts
+++ src/metaModel/propertyTypes.ts
@@ -1,3 +1,4 @@
 export type PropertyTypeJupType = "string" | "float" | "square-image" | "timestamp"
     | "image" | "address" | "integer" | "primary-key" | "icon" | "color" | "date" | "foreign-key"
-    | "belongs-to" | "has-many" | "has-many-paginated" | "boolean" | "surface" | "distance" | "point2d" | "text";
+    | "belongs-to" | "has-many" | "has-many-paginated" | "boolean" | "surface" | "distance"
+    | "point2d" | "text" | "currency";
